26ff6e7f4bf87ca2e1f89400d6c0fd05c6b2992d
Leonardo Pádua <leonardocpadua7@gmail.com>
2019-02-09 01:01:18 -0200

    Connecting to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    "mongodb+srv://roboto:AGsb5F3OJxZjvQWH@omnistack-week-cekqy.mongodb.net/test?retryWrites=true",{
+        useNewUrlParser: true
+    }
+)
 
 const app = express();
 
@@ -7,5 +14,5 @@ app.get('/', (req, res)=>{
 });
 
 app.listen(3000, ()=>{
-    console.log('Server started on port 3000');
+    console.log('Server started on port 3000 :)');
 })
