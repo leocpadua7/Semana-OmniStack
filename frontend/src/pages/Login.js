53288f03ca6529c0b2797a2d5883ca37cc40ff5f
Leonardo Pádua <leonardocpadua7@gmail.com>
2019-02-09 14:04:27 -0200

    backend and frontend integration via api

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import twitterLogo from '../twitter.svg'
+import './Login.css'
 
 export default class Login extends Component {
+  state={
+    username: '',
+  }
+  handleInputChange=(e)=>{
+    this.setState({username:e.target.value})
+  }
+  handleSubmit=(e)=>{
+    e.preventDefault();
+    const {username} = this.state;
+    if(!username.length) return;
+    localStorage.setItem('@GoTwitter:username', username)
+    this.props.history.push('/timeline')
+  }
+
   render() {
     return (
-        <h1>Login</h1>
+        <div className="login-wrapper">
+          <img src={twitterLogo} alt="GoTwitter" />
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              placeholder="Nome de Usuário"
+            />
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
     );
   }
 }
--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import './Timeline.css'
+import twitterLogo from '../twitter.svg'
+
+import api from '../services/api'
 
 export default class Timeline extends Component {
+state={
+  newTweet:'',
+}
+handleInputChange = (e) =>{
+  this.setState({newTweet:e.target.value})
+}
+handleNewTweet = async (e) =>{
+  if (e.keyCode !== 13) return;
+  const content=this.state.newTweet
+  const author = localStorage.getItem("@GoTwitter:username");
+
+  await api.post('tweets', {content, author})
+
+  this.setState({newTweet:''})
+}
+
   render() {
     return(
-        <h1>Timeline</h1>
+        <div className="timeline-wrapper">
+          <img height={24} src={twitterLogo} alt="GoTwitter" />
+
+          <form>
+            <textarea
+              value={this.state.newTweet}
+              onChange={this.handleInputChange}
+              onKeyDown={this.handleNewTweet}
+              placeholder="O que está acontecendo?"
+            />
+          </form>
+          <ul>
+
+          </ul>
+        </div>
     );
   }
 }
