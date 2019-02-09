e62176b654f25ca9f1fa898f3066fdc96319f4e8
Leonardo Pádua <leonardocpadua7@gmail.com>
2019-02-09 01:49:37 -0200

    routing, socket.io e controllers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(Server);
+
 mongoose.connect(
     "mongodb+srv://roboto:AGsb5F3OJxZjvQWH@omnistack-week-cekqy.mongodb.net/test?retryWrites=true",{
         useNewUrlParser: true
     }
-)
+);
 
-const app = express();
+app.use((req,res,next)=>{
+    req.io = io
+    
+    return next();
+})
 
-app.get('/', (req, res)=>{
-    return res.send('Hello World');
-});
+app.use(express.json());
+app.use(require('./routes'));
 
 app.listen(3000, ()=>{
     console.log('Server started on port 3000 :)');
